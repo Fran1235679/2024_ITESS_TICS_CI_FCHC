26676585733804ea01eb0faddf3c1f547ef882e5
franciscochavez <franchcornejo001@gmail.com>
2024-02-16 15:49:38 -0600

    Add class SerieAddTest

---
+++ src/CI/u1serie/SerieAddTest.java
@@ -0,0 +1,20 @@
+/*
+ * ITESS-TICS
+ * SEMESTRE ENERO-JUNIO 2024
+ * CALCULO INTEGRAL
+ *UNIDAD 1- TEOREMA FUNDAMENTAL DE CALCULO
+ * CALCULO DE APROXIMACION DE AREA
+ *FRANCISCO CHAVEZ CORNEJO
+ *16 FEBRERO 2024
+ */
+package CI.u1serie;
+
+
+
+public class SerieAddTest {
+    public static void main(String ... args){
+        SerieAdd sa = new SerieAdd(1,7);
+        
+       sa.compute();
+       sa.print();}
+}
